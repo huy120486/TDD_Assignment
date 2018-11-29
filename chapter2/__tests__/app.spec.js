af40453689bfc217c04dc36f0d1f043760977569
Huy Duong <huy.duong@employmenthero.com>
2018-11-29 21:57:00 +0700

    Refactor chapter 2 again
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- chapter2/__tests__/app.spec.js
+++ chapter2/__tests__/app.spec.js
@@ -1,18 +1,16 @@
 import { Dollar } from '../app';
 
-describe('Dollar', () => {
-  describe('multiplication', () => {
-    const five = new Dollar(5);
+describe('Multiplication', () => {
+  const five = new Dollar(5);
 
-    it('returns correct result when multiplying to 2', () => {
-      const product = five.times(2);
-      expect(product.amount).toEqual(10);
-    });
+  it('returns correct result when multiplying to 2', () => {
+    const product = five.times(2);
+    expect(product.amount).toEqual(10);
+  });
 
 
-    it('returns correct result when multiplying to 3', () => {
-      const product = five.times(3);
-      expect(product.amount).toEqual(15);
-    });
+  it('returns correct result when multiplying to 3', () => {
+    const product = five.times(3);
+    expect(product.amount).toEqual(15);
   });
 });
