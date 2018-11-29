5c7c366d5ce99566d71b950b13677d8d803f89a0
Huy Duong <huy.duong@employmenthero.com>
2018-11-29 21:57:33 +0700

    Refactor chapter 3 again
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- chapter3/__tests__/app.spec.js
+++ chapter3/__tests__/app.spec.js
@@ -1,28 +1,26 @@
 import { Dollar } from '../app';
 
-describe('Dollar', () => {
-  describe('Multiplication', () => {
-    const five = new Dollar(5);
+describe('Multiplication', () => {
+  const five = new Dollar(5);
 
-    it('returns correct result when multiplying to 2', () => {
-      const product = five.times(2);
-      expect(product.amount).toEqual(10);
-    });
-
-
-    it('returns correct result when multiplying to 3', () => {
-      const product = five.times(3);
-      expect(product.amount).toEqual(15);
-    });
+  it('returns correct result when multiplying to 2', () => {
+    const product = five.times(2);
+    expect(product.amount).toEqual(10);
   });
 
-  describe('Equality', () => {
-    it('returns true when comparing the same amounts', () => {
-      expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
-    });
 
-    it('returns false when comparing the different amounts', () => {
-      expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-    });
+  it('returns correct result when multiplying to 3', () => {
+    const product = five.times(3);
+    expect(product.amount).toEqual(15);
+  });
+});
+
+describe('Equality', () => {
+  it('returns true when comparing the same amounts', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
+  });
+
+  it('returns false when comparing the different amounts', () => {
+    expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
   });
 });
