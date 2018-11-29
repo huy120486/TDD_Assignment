5c2522ea6c2b2565b731b4d0a6b5180b97a316e1
Huy Duong <huy.duong@employmenthero.com>
2018-11-29 21:56:26 +0700

    Refactor chapter 1 again
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- chapter1/__tests__/app.spec.js
+++ chapter1/__tests__/app.spec.js
@@ -1,12 +1,10 @@
 import { Dollar } from '../app';
 
-describe('Dollar', () => {
-  describe('multiplication', () => {
-    const five = new Dollar(5);
+describe('Multiplication', () => {
+  const five = new Dollar(5);
 
-    it('returns correct result', () => {
-      five.times(2);
-      expect(five.amount).toEqual(10);
-    });
+  it('returns correct result', () => {
+    five.times(2);
+    expect(five.amount).toEqual(10);
   });
 });
