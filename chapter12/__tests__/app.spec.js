c3992909e85b1daa99c665e9dd4d9714b20ae6df
Huy Duong <huy.duong@employmenthero.com>
2018-12-01 07:52:26 +0700

    Add chapter 12
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

---
+++ chapter12/__tests__/app.spec.js
@@ -0,0 +1,56 @@
+import { Bank, Franc, Money } from '../app';
+
+describe('Multiplication', () => {
+  describe('Dollar', () => {
+    const five = Money.dollar(5);
+
+    it('returns correct result when multiplying to 2', () => {
+      expect(Money.dollar(10)).toEqual(five.times(2));
+    });
+
+
+    it('returns correct result when multiplying to 3', () => {
+      expect(Money.dollar(15)).toEqual(five.times(3));
+    });
+  });
+});
+
+describe('Equality', () => {
+  it('returns true when comparing the same amounts for Dollar', () => {
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+  });
+
+  it('returns false when comparing the different amounts for Dollar', () => {
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+  });
+
+  it('returns false when comparing Franc to Dollar', () => {
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
+  });
+});
+
+describe('Currency', () => {
+  it('returns correct currency for Dollar', () => {
+    expect(Money.dollar(1).currency()).toEqual('USD');
+  });
+
+  it('returns correct currency for Franc', () => {
+    expect(Money.franc(1).currency()).toEqual('CHF');
+  });
+});
+
+describe('DifferentClassEquality', () => {
+  it('returns true when comparing Money to Franc', () => {
+    expect(new Money(10, 'CHF').equals(new Franc(10, 'CHF'))).toBe(true);
+  });
+});
+
+describe('SimpleAddition', () => {
+  it('returns correct value for Dollar addition', () => {
+    const five = Money.dollar(5);
+    const sum = five.plus(five);
+    const bank = new Bank();
+    const reduced = bank.reduce(sum, 'USD');
+    expect(reduced).toEqual(Money.dollar(10));
+  });
+});
---
+++ chapter12/app.js
@@ -0,0 +1,40 @@
+export class Money {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  equals(money) {
+    return this._amount === money._amount
+      && this.currency() === money.currency();
+  }
+
+  static dollar(amount) {
+    return new Money(amount, 'USD');
+  }
+
+  static franc(amount) {
+    return new Money(amount, 'CHF');
+  }
+
+  currency() {
+    return this._currency;
+  }
+
+  times(multiplier) {
+    return new Money(this._amount * multiplier, this._currency);
+  }
+
+  plus(addend) {
+    return new Money(this._amount + addend._amount, this._currency);
+  }
+}
+
+export class Franc extends Money {
+}
+
+export class Bank {
+  reduce(source, to) {
+    return Money.dollar(10);
+  }
+}
