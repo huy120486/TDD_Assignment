77f9bb5b447f2b5a914fdf760b44c8fa8295e325
Son Trinh <son.trinh@employmenthero.com>
2018-12-20 15:36:47 +0700

    split step chap 3

--- chapter3/__tests__/app.spec.js
+++ chapter3/__tests__/app.spec.js
@@ -8,13 +8,20 @@ describe('Multiplication', () => {
     expect(product.amount).toEqual(10);
   });
 
-
   it('returns correct result when multiplying to 3', () => {
     const product = five.times(3);
     expect(product.amount).toEqual(15);
   });
 });
 
+// Step 1
+// describe('Equality', () => {
+//   it('returns true when comparing the same amounts', () => {
+//     expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
+//   });
+// });
+
+// Step 2
 describe('Equality', () => {
   it('returns true when comparing the same amounts', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
