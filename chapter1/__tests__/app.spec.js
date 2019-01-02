d0852cd1d4da7db741b5c0c7ba1030857247b364
Huy Duong <huy.duong@employmenthero.com>
2019-01-02 22:18:07 +0700

    Update code for chapter 1
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- chapter1/__tests__/app.spec.js
+++ chapter1/__tests__/app.spec.js
@@ -2,17 +2,6 @@ import { Dollar } from '../app';
 
 // Step 1
 
-// describe('Multiplication', () => {
-//   const five = new Dollar(5);
-
-//   it('returns correct result', () => {
-//     five.times(2);
-//     expect(five.amount).toEqual(10);
-//   });
-// });
-
-// Step 2
-
 describe('Multiplication', () => {
   const five = new Dollar(5);
 
--- chapter1/app.js
+++ chapter1/app.js
@@ -1,18 +1,49 @@
 // Step 1
-
 // export class Dollar {
-
 // }
 
 // Step 2
 // export class Dollar {
+//   times() {}
+// }
+
+// Step 3
+// export class Dollar {
+//   constructor() {
+//     this.amount = 10;
+//   }
+//   times() {}
+// }
+
+// Step 4
+// export class Dollar {
+//   constructor() {
+//     this.amount = 5 * 2;
+//   }
+//   times() {}
+// }
+
+// Step 5
+// export class Dollar {
 //   constructor(amount) {
 //     this.amount = amount;
 //   }
+//   times() {
+//     this.amount = this.amount * 2;
+//   }
 // }
 
-// step3
+// Step 6
+// export class Dollar {
+//   constructor(amount) {
+//     this.amount = amount;
+//   }
+//   times(multiplier) {
+//     this.amount = this.amount * multiplier;
+//   }
+// }
 
+// Step 7
 export class Dollar {
   constructor(amount) {
     this.amount = amount;
@@ -22,4 +53,3 @@ export class Dollar {
     this.amount *= multiplier;
   }
 }
-// ....
